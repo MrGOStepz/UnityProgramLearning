9fadc6d9bb9544dd1323e71228e65e60c2ea4f34
Warawat Suksakulwat <mrgostepz@gmail.com>
2025-12-12 20:14:42 +0700

    Dine with Challenge 1

--- StartYour3DEngines/StartYour3DEngines/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ StartYour3DEngines/StartYour3DEngines/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(60f, 25f, 30f);
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +14,8 @@ public class FollowPlayerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = plane.transform.position + offset;
+        transform.position = plane.transform.position + new Vector3(60f, 25f, 30f);
     }
 }
